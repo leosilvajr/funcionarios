d88d565e2158ae2ad0440af218f994d4c7ad1e10
Leonardo <dp.ti@palinemartins.com.br>
2024-11-13 09:40:34 -0300

    Fisrt Commit

---
+++ Funcionarios/Controllers/FuncionariosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Funcionarios.Models;
+
+namespace EmployeeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FuncionariosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Funcionario>> GetFuncionarios()
+        {
+            var funcionarios = new List<Funcionario>
+            {
+                new Funcionario(Guid.NewGuid(), "Leonardo", "Silva", "Desenvolvedor", 500.50M),
+                new Funcionario(Guid.NewGuid(), "Fabio", "Faria", "Gestor de Projetos", 450.75M),
+                new Funcionario(Guid.NewGuid(), "Victor", "Franca", "Desenvolvedor", 700.00M),
+                new Funcionario(Guid.NewGuid(), "Gabriel", "Lourenço", "Automação", 600.25M),
+                new Funcionario(Guid.NewGuid(), "Yohanna", "Vavra", "Analista de Qualidade", 600.25M),
+                new Funcionario(Guid.NewGuid(), "Alexandre", "Roldao", "Desenvolvedor", 600.25M)
+            };
+
+            return Ok(funcionarios);
+        }
+    }
+}
---
+++ Funcionarios/Models/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿namespace Funcionarios.Models
+{
+    public class Funcionario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Funcao { get; set; }
+        public decimal Salario { get; set; }
+
+        public Funcionario(Guid id, string nome, string sobrenome, string funcao, decimal salario)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Funcao = funcao;
+            Salario = salario;
+        }
+    }
+}
